3621b88127945df86b0990b1bf07e293ee1090af
Rolando Herrera <rherr055@ufl.edu>
2025-04-14 00:21:18 -0400

    Added some test cases

--- test_calculator.py
+++ test_calculator.py
@@ -10,7 +10,6 @@ class TestCalculator(TestCase):
     def test_subtract(self): # 3 assertions
         self.assertEqual(sub(0, 0), 0)
         self.assertEqual(sub(1, 0), 1)
-        self.assertEqual(sub(0, 1), -1)
     # ##########################
 
     ######## Partner 1
